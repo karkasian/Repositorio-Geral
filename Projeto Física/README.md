7f3795d1ad9f4ad4077599189ae1741a967d5327
Jhordan Silveira de Borba <jhordandecacapava@gmail.com>
2018-06-18 17:51:31 -0300

    readme

---
+++ Projeto Física/README.md
@@ -0,0 +1,14 @@
+# Projeto Física
+
+##Dados de treinamento
+
+
+
+Projectile speed: 17.5 meters per secon
+Area of effect: 2 meters radius
+
+x=0-10;1
+o=0-45;1
+v=12.5-22.5;0,5
+
+10x*45*20*
--- Projeto Física/dados.cpp
+++ Projeto Física/dados.cpp
@@ -7,26 +7,30 @@ Sobre o projeto:		https://github.com/SapoGitHub/Repositorio-Geral/wiki/Projeto-F
 2018
 */
 
-#include <stdio.h>
+#include <stdio.h>		//Biblioteca padrão para comandos de entrada e saída
+#include<math.h>		//Biblioteca com funções matemáticas
  
 int main ()
 {
    FILE * f;						//Declara arquivo
-   int vel,x,c;
-   long int t,quant;
-   vel=10;							//Velocidade
-   quant=100000;					//Quantidade de dados a serem gerados
+   float theta,v,vx,vy,x;
    f = fopen ("dados.txt","w");		//Abre arquivo
-	c=0;
-   for (t=101;t<101+quant;t++)	//Vamos gerar dados
-   {
-  	x=vel*t;						//Função do MRU
- 	  	fprintf(f,"%d\n%d",x,t);		//Salvar no arquivo
-   	if (t!=101+quant-1){
-   		fprintf(f,"\n");
-	   }
-   }
    
+   v=10;
+   theta=3.14/4;
+   vx=v*cos(theta);
+   vy=v*sin(theta);
+
+   for (x=0;x<10;x=x+0.1)				//Vamos gerar dados
+   {
+									//Função do MRU
+ 	  	fprintf(f,"\n");		//Salvar no arquivo
+ 	  	printf("%f \n",x);
+   	if (x!=10-1)
+	   {
+   			fprintf(f,"\n");
+	   }
+   }   
    fclose (f);						//Fechar o arquivo
    return 0;
 }
