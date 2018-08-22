992e1ab1b3f974aab48af8a9e46b8077f33a943a
Jhordan Silveira de Borba <39818915+SapoGitHub@users.noreply.github.com>
2018-08-22 20:44:06 -0300

    algoritmo de booth

---
+++ Arquitetura/README.md
@@ -0,0 +1,63 @@
+﻿# Arquitetura de computadores
+
+**Problema 2.1**:
+Considere que A = A(1), A(2),... , A(1 000) e B = B(1), B(2),..., B(1 000) sejam dois vetores (arrays unidimensionais) compostos de 3 números em cada um, que são somados para formar um array C tal que C(I) = A(I) + B(I) para I = 1,2,3. Usando o conjunto de instruções do IAS, escreva um programa para esse problema. 
+
+[Código](Problema 2.1.asm) em linguagem de montagem.
+
+** Problema 2.4**:
+Interprete e escreva em linguagem de montagem:
+```
+08A 010FA210FB
+08B 010FA0F08D
+08C 020FA210FB
+```
+
+Reescrevendo de forma mais legível:
+```
+08A [01 0FA] [21 0FB]
+08B [01 0FA] [0F 08D]
+08C [02 0FA] [21 0FB]
+```
+Então para a primeira linha guarda seu conteúdo no endereço 08A Hex (138 Dec). Seu conteúdo tem 2 palavras:
+1. [01 0FA]
+2. [21 0FB]
+A primeira palavra, traz um opcode e um endereço:
+1. 01 Hex (1 Dec, 0000 0001 Bin): Load M(X): transfere o conteúdo de X para o AC
+2. 0FA Hex (250 Dec): Endereço de memória.
+
+Podemos reescrever então como [01 0FA]=LOAD M(0FA). De forma análoga, temos para a segunda palavra 21 Hex(33 Dec, 00010 0001 Bin). Esse opcode é STOR M(X), que implica em transferir o conteúdo de AC para X.
+
+Então a primeira linha do código contém de forma descritiva:
+- Copia o conteúdo de 0FA para AC, e depois de AC para 0FB.
+
+Então fazendo o mesmo para as outras linhas, temos:
+- Copia o conteúdo de 0FA para AC, e depois de AC para 0FB.
+- Copia novamente o conteúdo de 0FA para AC e se o número for positivo, apanha a próxima instrução da metade esquerda de 08D.
+- Então transferimos o valor de 0FA com o sinal invertido para AC e salvamos em 0FB.
+
+Fizemos algumas mudanças:
+- Declaramos um valor em 0FA (25 decimal);
+- Substituímos os endereços 08A, 08B e 08C por 000, 001 e 08D respectivamente;
+- Adicionamos um comando para pular para um endereço que não existe em 08E.
+
+Então agora nosso código:
+- Armazena o número 25 em 0FA;
+- Copia 25 para o AC, e depois passa do AC para 0FB;
+- Copia 25 novamente para o AC e como é positivo apanha a próxima instrução na metade esquerda de 08D;
+- Salvamos 25 com sinal contrário (-25) em AC e do AC guardamos em 0FB;
+- Buscamos a próxima instrução em um endereço inexistente (1024 em decimal) causando a interrupção do programa:
+
+Temos o código em [linguagem de máquina](Problema 2.4.obj) e [linguagem de montagem](Problema 2.4.asm).
+
+**Adição de inteiros**:
+A adição de inteiros em binário seguem algumas regras simples:
+- 0+0=0;
+- 0+1=1;
+- 1+0=1;
+- 1+1=0 (e 'vai 1' para o dígito de ordem superior)
+- 1+1+1=1 (e 'vai 1' para o dígito de ordem superior)
+
+E temos uma regra simples para determinar a ocorrência de overflow: se os dois números somados tem o mesmo sinal, ocorre overflow se o resultado possuir o sinal oposto.
+
+Escrevemos um [código em python](overflow.py) para isso.
---
+++ Arquitetura/overflow.py
@@ -0,0 +1,80 @@
+#Vamos definir os registradores
+class
+
+#Algoritmos relacionados a adição
+class adicao:
+    
+    #Checar se ouve overflow
+    def overflow(um,dois,res):
+        #um     - Primeiro número somado
+        #dois   - Segundo número somado
+        #res    - Resultado da soma
+        
+        if (um[0]!=dois[0]):    #Se os sinais são diferentes
+            return res          #Não houve overflow
+        else:                   #Se são iguais
+            if (um[0]==res[0]): #Se a resposta tem o mesmo sinal dos números
+                return res      #Não houve overflow
+            else:               #Se o sinal é diferente
+                return 'overflow'#Houve
+    
+    #Regras da adição
+    def regras(um,dois):
+        #um     - Primeiro bit
+        #dois   - Segundo bit
+
+        #Vamos trabalhar com os casos:
+        if (um==0 and dois ==0):    #0+0
+            return (0,0)
+        elif (um==0 and dois ==1):  #0+1
+            return (1,0)
+        elif (um==1 and dois==0):     #1+0
+            return (1,0)
+        else:
+            return (0,1)
+        
+    #Para obtermos um bit da soma
+    def bit (um, dois, e):
+        #um     - Primeiro bit
+        #dois   - Segundo bit
+        #e      - Bit resultado de soma anterior
+
+        if(e==0):   #Se não temos de um anterior, apenas somamos:
+            (res,elev)=adicao.regras(um,dois)
+        else:       #Se temos
+            if (um==0):      #E o primeiro é zero
+                (res,elev)=adicao.regras(e,dois)    #Somamos ao segundo
+            elif(dois==0):  #Se não mas o segundo é:
+                (res,elev)=adicao.regras(e,um)      #Somamos ao primeiro
+            else:           #Se nenhum é zero
+                (res,elev1)=adicao.regras(e,um)      #Somamos ao primeiro
+                (res,elev2)=adicao.regras(res,dois)   #E somamos o resultado ao segundo
+                (elev,des)=adicao.regras(elev1,elev2)#E retornamos a soma dos elevados
+        return (res,elev)
+        
+    
+    #Operação de fato
+    def operacao (um,dois):
+        #um     - Primeiro número
+        #dois   - Segundo número
+        
+        tam=len(um)                     #Vamos pegar o tamanho
+        res=[]                          #Vamos guardar o resultado
+        elev=0                          #Vamos guardar o valor elevado
+        for bit in range(tam-1,-1,-1):  #Vamos percorrer bit a bit o número
+            (r,elev)=adicao.bit(int(um[bit]),int(dois[bit]),elev)    #Vamos somar todos os bits
+            res.append(r)               #Salvamos o resultado
+        #Agora vamos montar o resultado
+        resposta=''                     #Onde vamos guardar a resposta
+        for bit in range (tam-1,-1,-1):
+            resposta=resposta+str(res[bit])
+
+        #Checamos o overflow
+        resposta=adicao.overflow(um,dois,resposta)
+        #Retornamos a resposta
+        return (resposta)
+
+        
+
+resposta=adicao.operacao('0011','0100')
+print(resposta)
